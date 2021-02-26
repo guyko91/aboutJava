4bf37667d37c112e50d937713932cdf42fa66d23
guyko91 <guyko91@gmail.com>
2021-02-26 16:40:09 +0900

    completablefuture 추가.

---
+++ src/temp/App.java
@@ -0,0 +1,11 @@
+package temp;
+
+public class App {
+    public static void main(String[] args) {
+
+        String test = "01";
+        int result = Integer.parseInt(test);
+        System.out.println(result);
+    }
+
+}
